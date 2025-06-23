f6381eb9bc74bdfcefc87744dbd0f8a22877c5ea
hans <hans.lassen@trustworks.dk>
2025-06-23 12:36:05 +0200

    Fixed small problems

--- src/main/resources/db/migration/V65__Conference_add_text_fields.sql
+++ src/main/resources/db/migration/V65__Conference_add_text_fields.sql
@@ -1,6 +1,6 @@
 -- Add description and text columns to conferences table
 ALTER TABLE conferences
-    ADD COLUMN description VARCHAR(255) NULL AFTER active,
-    ADD COLUMN note_text VARCHAR(255) NULL AFTER description,
-    ADD COLUMN consent_text VARCHAR(255) NULL AFTER note_text,
-    ADD COLUMN thanks_text VARCHAR(255) NULL AFTER consent_text;
+    ADD COLUMN description TEXT NULL AFTER active,
+    ADD COLUMN note_text TEXT NULL AFTER description,
+    ADD COLUMN consent_text TEXT NULL AFTER note_text,
+    ADD COLUMN thanks_text TEXT NULL AFTER consent_text;
