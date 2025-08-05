062709f0d04ab26fdb98977eae44a63ed97fbaa9
dabster2000 <hans@godfather.dk>
2025-08-05 18:45:26 +0200

    Use Quarkus REST client and enable Mutiny

--- src/main/java/dk/trustworks/intranet/aggregates/SSEResource.java
+++ src/main/java/dk/trustworks/intranet/aggregates/SSEResource.java
@@ -8,12 +8,14 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import lombok.extern.jbosslog.JBossLog;
 import org.jboss.resteasy.reactive.RestStreamElementType;
 
 import static dk.trustworks.intranet.messaging.emitters.AggregateMessageEmitter.BROWSER_EVENT;
 
 @Path("/sse")
 @ApplicationScoped
+@JBossLog
 public class SSEResource {
 
     private final BroadcastProcessor<String> broadcastProcessor = BroadcastProcessor.create();
@@ -23,13 +25,13 @@ public class SSEResource {
     @RestStreamElementType(MediaType.TEXT_PLAIN)
     //@SseElementType(MediaType.TEXT_PLAIN)
     public Multi<String> stream() {
-        System.out.println("SSEResource.stream");
+        log.info("SSEResource.stream");
         return broadcastProcessor;
     }
 
     @ConsumeEvent(BROWSER_EVENT)
     public void consume(String aggregateUUID) {
-        System.out.println("SSEResource.consume: " + aggregateUUID);
+        log.infof("SSEResource.consume: %s", aggregateUUID);
         broadcastProcessor.onNext(aggregateUUID);
     }
 
