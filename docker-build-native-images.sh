108dce8d310234117af7f20f1ce9ee1a4a449b5b
hans <hans.lassen@trustworks.dk>
2025-10-17 20:20:16 +0200

    Update build scripts and native build configuration
    - Refactor `docker-build-native-images.sh` to conditionally build Maven projects based on build type (native/JVM).
    - Upgrade `Dockerfile.multistage` to use `ubi9-quarkus-mandrel-builder-image` and enable `-Pbuild

--- docker-build-native-images.sh
+++ docker-build-native-images.sh
@@ -6,7 +6,10 @@ targetenv="prod"
 # linux/amd64 (prod), linux/arm64 (latest)
 targetplatform="linux/amd64"
 
-mvn clean package -f pom.xml
+# For native builds, the Dockerfile.multistage handles the Maven build
+# For JVM builds, we need to build first
+if [ $buildtype = 'jvm' ]; then mvn clean package -f pom.xml; fi
+
 if [ $buildtype = 'native' ]; then docker build -f src/main/docker/Dockerfile.multistage -t trustworks/twservices:$targetenv .; fi
 if [ $buildtype = 'jvm' ]; then docker build --platform $targetplatform -f src/main/docker/Dockerfile.jvm -t trustworks/twservices:$targetenv .; fi
 docker push trustworks/twservices:$targetenv
--- src/main/java/dk/trustworks/intranet/dao/crm/model/Client.java
+++ src/main/java/dk/trustworks/intranet/dao/crm/model/Client.java
@@ -1,17 +1,19 @@
 package dk.trustworks.intranet.dao.crm.model;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
 
-import jakarta.persistence.*;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.UUID;
+import java.time.LocalDateTime;
+import java.util.*;
 
-@Data
-@EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = false)
+@Getter
+@Setter
+@ToString
 @Entity
 @Table(name = "client")
 public class Client extends PanacheEntityBase {
@@ -20,8 +22,8 @@ public class Client extends PanacheEntityBase {
     private String uuid;
     private boolean active;
     private String contactname;
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date created;
+
+    private LocalDateTime created;
     private String name;
     private String crmid;
     private String accountmanager;
@@ -32,18 +34,33 @@ public class Client extends PanacheEntityBase {
     public Client() {
         this.projects = new ArrayList<>();
         this.clientdata = new ArrayList<>();
-        this.created = new Date();
+        this.created = LocalDateTime.now();
     }
 
     public Client(String contactname, String name) {
         uuid = UUID.randomUUID().toString();
         this.active = true;
         this.contactname = contactname;
-        this.created = new Date();
+        this.created = LocalDateTime.now();
         this.name = name;
         this.crmid = "";
         this.projects = new ArrayList<>();
         this.clientdata = new ArrayList<>();
     }
 
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Client client = (Client) o;
+        return getUuid() != null && Objects.equals(getUuid(), client.getUuid());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
